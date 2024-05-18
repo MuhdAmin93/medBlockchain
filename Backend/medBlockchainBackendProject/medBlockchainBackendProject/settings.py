a543af0bc676a653f97ce8cd7a48e2726047a837
matrix232 <egortop15@gmail.com>
2024-05-18 21:42:42 +0300

    added new authentication app

--- Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
+++ Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from medBlockchain.Backend.config import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psycopg2',
+    'authapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'medBlockchainBackendProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'Users',
+        'USER' : "BananaAdmin",
+        'PASSWORD' : PASS_DATABASE,
+        'HOST' : '127.0.0.1',
+        'PORT' : "5432",
     }
 }
 
