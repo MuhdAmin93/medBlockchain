fc9f4119d400e4abe85133b0df5ded2d25739cf0
matrix232 <egortop15@gmail.com>
2024-05-18 22:11:41 +0300

    created models

--- Backend/medBlockchainBackendProject/authapp/models.py
+++ Backend/medBlockchainBackendProject/authapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    lastname = models.CharField('lstname', max_length=50)
+    firstname = models.CharField('firstname', max_length=50)
+    patronymic = models.CharField('patronymic', max_length=50)
+    login = models.CharField('login', max_length=100)
+    password = models.CharField('password', max_length=100)
+    mail = models.CharField('password', max_length=100)
+
+    def __str__(self):
+        return self.login
--- Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
+++ Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from medBlockchain.Backend.config import *
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'medBlockchainBackendProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'Users',
-        'USER' : "BananaAdmin",
-        'PASSWORD' : PASS_DATABASE,
-        'HOST' : '127.0.0.1',
-        'PORT' : "5432",
+        'NAME': 'Users',
+        'USER': "BananaAdmin",
+        'PASSWORD': 'brrtsdf',
+        'HOST': '127.0.0.1',
+        'PORT': "5432",
     }
 }
 
