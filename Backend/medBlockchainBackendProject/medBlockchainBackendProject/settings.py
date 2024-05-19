9f6c7c41762b18e3ba52ad33bd2a21be5dc95965
matrix232 <egortop15@gmail.com>
2024-05-19 09:13:40 +0300

    correcting database

--- Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
+++ Backend/medBlockchainBackendProject/medBlockchainBackendProject/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Users',
-        'USER': "BananaAdmin",
-        'PASSWORD': 'brrtsdf',
+        'USER': "postgres",
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': "5432",
     }
